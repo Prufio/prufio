ff79ccda0936519bd037b0872cb92edc2e14cc7e
valinsmyth <valinsmyth@gmail.com>
2021-03-31 11:06:13 -0400

    made 'powered by' block global, added 'featured on' block

--- assets/popup-plugin/script.js
+++ assets/popup-plugin/script.js
@@ -1,2 +1,2 @@
 
-(function(a){a(document).ready(function(){if(!a("html").hasClass("is-builder"))a(".mbr-gallery-item").on("click","a",function(b){a(this)[0].hasAttribute("data-target")&&""!==a(this).attr("data-target")&&a("#"+a(this).attr("data-target").replace("#","")).modal("show")})})})(jQuery);
+(function(a){a(document).ready(function(){a("html").hasClass("is-builder")||(a(".mbr-gallery-item").on("click","a",function(b){a(this)[0].hasAttribute("data-target")&&""!==a(this).attr("data-target")&&a("#"+a(this).attr("data-target").replace("#","")).modal("show")}),setTimeout(function(){a(".mbr-popup .modal-content *").removeClass("hidden animate__animated animate__delay-1s animate__fadeInUp")},0))})})(jQuery);
