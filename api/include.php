b9348d3ce908adb544d5c64db001930c8cda6fe8
Valin A. Smyth <valinsmyth@gmail.com>
2022-09-08 11:11:37 -0600

    deleted api directory
    We will be hosting it elsewhere

--- api/include.php
+++
@@ -1,7 +0,0 @@
-<?php
-$api_key_polygo = 'F9GZM22PSKBBCI3YZ21CK8B3DC9C5DRKXF';
-$api_key_erc20 = 'S98XYH4AJZCGGVJAEM6667R73S3TURX6HX';
-
-//Website URL with Folder Name --> Don't put / at the end <--
-$website_url = 'https://pruf.io/api';
-?>
